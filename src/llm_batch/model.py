c297b0ba50571694d3292eff7d6cbfdc8d060952
Seng Tak <sengtak@hotmail.com>
2023-11-23 09:40:56 +0800

    updated openai API version to 1.3.0

--- src/llm_batch/model.py
+++ src/llm_batch/model.py
@@ -1,8 +1,13 @@
 import logging
 import os
+from typing import Any
 
 import dotenv
 import openai
+from openai import AzureOpenAI, OpenAI
+
+#client = AzureOpenAI(api_key=os.environ["OPENAI_API_KEY"],
+#api_version=os.environ["OPENAI_API_VERSION"])
 import vertexai  # type: ignore
 from langchain.llms import AzureOpenAI
 from vertexai.language_models import TextGenerationModel  # type: ignore
@@ -12,10 +17,11 @@ dotenv.load_dotenv()
 GCP_PROJECT = os.environ["GCP_PROJECT"]
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-openai.api_key = os.environ["OPENAI_API_KEY"]
-openai.api_base = os.environ["OPENAI_API_BASE"]
-openai.api_type = os.environ["OPENAI_API_TYPE"]
-openai.api_version = os.environ["OPENAI_API_VERSION"]
+
+# TODO: The 'openai.api_base' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(api_base=os.environ["OPENAI_API_BASE"])'
+# openai.api_base = os.environ["OPENAI_API_BASE"]
+
+
 
 
 logger = logging.getLogger(__name__)
@@ -45,6 +51,31 @@ def get_gpt_4():
         engine="gpt-4-us",
     )
 
+class OpenAIGPT35:
+    def __init__(self):
+        self.model = OpenAI(base_url=os.environ["OPENAI_API_BASE"], api_key=os.environ["OPENAI_API_KEY"])
+    
+    def __call__(self, prompt) -> Any:
+        #list of models https://platform.openai.com/docs/models/gpt-3-5
+        stream = self.model.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": "You are a helpful assistant."},
+                      {"role": "user", "content": prompt}],
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            stream=True)
+        
+        
+        #print(f"debug {response} {self.model.base_url}")
+        response = ""
+
+        for part in stream:
+            add = part.choices[0].delta.content or ""
+            response += add
+
+        return response
+
 
 def get_gpt_35_turbo():
     # Different approach than gpt-4, as for gpt-3 we get an error:
@@ -52,19 +83,16 @@ def get_gpt_35_turbo():
     logger.info("Loading LLM 'gpt-35-turbo'")
 
     # this allows running the returned llm with llm("your prompt")
-    return (
-        lambda prompt: openai.ChatCompletion.create(
-            model="gpt-35-turbo",
-            engine="gpt-35-turbo-us",
-            messages=[{"role": "user", "content": prompt}],
-            temperature=0.7,
-            max_tokens=256,
-            top_p=1,
-        )
-        .choices[0]
-        .message.content
-    )
-
+    #return (
+    #    lambda prompt: client.chat.completions.create(model="gpt-35-turbo",
+    #    messages=[{"role": "user", "content": prompt}],
+    #    temperature=0.7,
+    #    max_tokens=256,
+    #    top_p=1)
+    #    .choices[0]
+    #    .message.content
+    #)
+    return OpenAIGPT35()
 
 def get_palm():
     logger.info("Loading LLM 'text-bison@001'")
